b78f1b722e5bf99f71baa9ec8ebc00fdab45cf6c
Mnesikos <mnesikos@gmail.com>
2023-08-26 00:31:11 -0400

    actually fix chin, budgie, maned wolf babies

--- src/main/java/com/github/kmfisk/zawaessentials/client/render/entity/BudgerigarRenderer.java
+++ src/main/java/com/github/kmfisk/zawaessentials/client/render/entity/BudgerigarRenderer.java
@@ -1,13 +1,11 @@
 package com.github.kmfisk.zawaessentials.client.render.entity;
 
-import com.github.kmfisk.zawaessentials.ZawaEssentials;
 import com.github.kmfisk.zawaessentials.client.model.BudgerigarModel;
 import com.github.kmfisk.zawaessentials.entity.BudgerigarEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 import org.zawamod.zawa.client.renderer.entity.ZawaMobRenderer;
-import org.zawamod.zawa.resources.EntityStatsManager;
 
 public class BudgerigarRenderer extends ZawaMobRenderer<BudgerigarEntity, BudgerigarModel> {
     public BudgerigarRenderer(EntityRendererManager rendererManager) {
@@ -27,17 +25,6 @@ public class BudgerigarRenderer extends ZawaMobRenderer<BudgerigarEntity, Budger
 
     @Override
     public ResourceLocation getBabyTexture(BudgerigarEntity entity, int variant) {
-        if (variant >= entity.getWildVariants()) {
-            String variantName = EntityStatsManager.INSTANCE.getStats(entity).getCaptiveVariantsList().get(variant - entity.getWildVariants());
-            return new ResourceLocation(ZawaEssentials.MOD_ID, "textures/entity/budgerigar/budgerigar_" + variantName + ".png");
-        } else return this.babyTexture != null ? this.babyTexture : this.babyTextures[variant];
-    }
-
-    @Override
-    public void setupBabyTextures(BudgerigarEntity entity) {
-        int variantCount = entity.getTotalVariants();
-        babyTextures = new ResourceLocation[variantCount];
-        for (int i = 0; i < variantCount; i++)
-            babyTextures[i] = new ResourceLocation(ZawaEssentials.MOD_ID, "textures/entity/budgerigar/budgerigar_" + (i + 1) + ".png");
+        return this.babyTexture != null ? this.babyTexture : this.babyTextures[variant];
     }
 }
--- src/main/java/com/github/kmfisk/zawaessentials/client/render/entity/ManedWolfRenderer.java
+++ src/main/java/com/github/kmfisk/zawaessentials/client/render/entity/ManedWolfRenderer.java
@@ -1,13 +1,11 @@
 package com.github.kmfisk.zawaessentials.client.render.entity;
 
-import com.github.kmfisk.zawaessentials.ZawaEssentials;
 import com.github.kmfisk.zawaessentials.client.model.ManedWolfModel;
 import com.github.kmfisk.zawaessentials.entity.ManedWolfEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 import org.zawamod.zawa.client.renderer.entity.ZawaMobRenderer;
-import org.zawamod.zawa.resources.EntityStatsManager;
 
 public class ManedWolfRenderer extends ZawaMobRenderer<ManedWolfEntity, ManedWolfModel<ManedWolfEntity>> {
     public ManedWolfRenderer(EntityRendererManager rendererManager) {
@@ -21,17 +19,6 @@ public class ManedWolfRenderer extends ZawaMobRenderer<ManedWolfEntity, ManedWol
 
     @Override
     public ResourceLocation getBabyTexture(ManedWolfEntity entity, int variant) {
-        if (variant >= entity.getWildVariants()) {
-            String variantName = EntityStatsManager.INSTANCE.getStats(entity).getCaptiveVariantsList().get(variant - entity.getWildVariants());
-            return new ResourceLocation(ZawaEssentials.MOD_ID, "textures/entity/maned_wolf/maned_wolf_" + variantName + ".png");
-        } else return this.babyTexture != null ? this.babyTexture : this.babyTextures[variant];
-    }
-
-    @Override
-    public void setupBabyTextures(ManedWolfEntity entity) {
-        int variantCount = entity.getTotalVariants();
-        babyTextures = new ResourceLocation[variantCount];
-        for (int i = 0; i < variantCount; i++)
-            babyTextures[i] = new ResourceLocation(ZawaEssentials.MOD_ID, "textures/entity/maned_wolf/maned_wolf_" + (i + 1) + ".png");
+        return this.babyTexture != null ? this.babyTexture : this.babyTextures[variant];
     }
 }
